0ba3f2650ca83b5edd5bc79c2f7ba3b13b8e87d6
senorkumar <sdkumar@hmc.edu>
2023-11-02 16:19:22 -0700

    data analysis

---
+++ data_analysis.py
@@ -0,0 +1,62 @@
+import json
+import ipdb
+import sklearn.metrics as metrics
+import matplotlib.pyplot as plt
+
+
+def accuracy_score(gt_labels, pred_labels):
+    return metrics.accuracy_score(gt_labels, pred_labels)
+
+
+def balanced_accuracy_score(gt_labels, pred_labels):
+    return metrics.balanced_accuracy_score(gt_labels, pred_labels)
+
+
+def precision_score(gt_labels, pred_labels):
+    return metrics.precision_score(gt_labels, pred_labels)
+
+
+def recall_score(gt_labels, pred_labels):
+    return metrics.recall_score(gt_labels, pred_labels)
+
+
+with open("results_copy.json", "r") as file:
+    results = json.load(file)
+
+gt_labels = []
+pred_labels = []
+for key, value in results.items():
+    gt_labels.append(value["gt_label"])
+    pred_labels.append(value["predicted_label"])
+
+# the x axis is just the total number of elements the metric is being run on
+x = list(range(1, len(gt_labels) + 1))
+
+# metrics will be stored in these lists
+accuracy_score_results = []
+balanced_accuracy_score_results = []
+precision_score_results = []
+recall_score_results = []
+
+
+# run the metric on the first element, then the first two elements, then the first three elements, etc.
+for i in range(1, len(gt_labels) + 1):
+    accuracy_score_results.append(accuracy_score(gt_labels[:i], pred_labels[:i]))
+    balanced_accuracy_score_results.append(
+        balanced_accuracy_score(gt_labels[:i], pred_labels[:i])
+    )
+    precision_score_results.append(precision_score(gt_labels[:i], pred_labels[:i]))
+    recall_score_results.append(recall_score(gt_labels[:i], pred_labels[:i]))
+
+
+# create a separate plot for each metric
+fig, axs = plt.subplots(4, 1)
+axs[0].plot(x, accuracy_score_results)
+axs[0].set_title("Accuracy Score")
+axs[1].plot(x, balanced_accuracy_score_results)
+axs[1].set_title("Balanced Accuracy Score")
+axs[2].plot(x, precision_score_results)
+axs[2].set_title("Precision Score")
+axs[3].plot(x, recall_score_results)
+axs[3].set_title("Recall Score")
+plt.show()
